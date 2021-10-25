9ff357bcc8ffad718fbd06ac06cc87327c2c3646
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-10-25 19:45:02 +0800

    將 Route 取名為 hello.index

--- routes/web.php
+++ routes/web.php
@@ -37,12 +37,15 @@ use Illuminate\Support\Facades\Route;
     Route::get('hello/{name}',function($name){
         return'Hello,'.$name;
         });
+        */
 
     Route::get('hello/{name?}',function($name='Everybody'){
         return'Hello,'.$name;
-         });
-*/
+         })->name('hello.index');
+
 //增加新Route
-    Route::get('hello/{name}', function($name){
+   /*  Route::get('hello/{name}', function($name){
         return 'Hello, '.$name.'. Thanks you!';
         });
+        */
+        
